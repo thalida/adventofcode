bbb4544714d605498ff42ce77148fae96df7f72d
Thalida Noel <thalida.c.noel@gmail.com>
2018-12-04 12:46:29 -0500

    Setup Day Solution Template

---
+++ 2018/_d/1/run.py
@@ -0,0 +1,17 @@
+# https://adventofcode.com/2018/day/_d
+
+import os
+import functools
+import re
+from collections import defaultdict
+
+script_dir = os.path.dirname(__file__)
+inputs = []
+with open(os.path.join(script_dir,'input.txt'), 'r') as f:
+  inputs = f.read().splitlines()
+
+def run(inputs):
+    output = 'day/_d'
+    return output
+
+print(run(inputs))
