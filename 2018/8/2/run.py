380b0350c87e24fa9f16514d30ad4ed39d520068
Thalida Noel <thalida.c.noel@gmail.com>
2018-12-08 16:41:17 -0500

    Day 8: Part 2

---
+++ 2018/8/2/run.py
@@ -0,0 +1,57 @@
+# https://adventofcode.com/2018/day/8
+
+import os
+from collections import defaultdict
+from pprint import pprint
+
+script_dir = os.path.dirname(__file__)
+inputs = []
+with open(os.path.join(script_dir,'input.txt'), 'r') as f:
+  inputs = f.read().splitlines()
+
+# inputs = ["2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"]
+# 2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
+#                 2 (3) [1, 1, 2]              
+# 0 (3) [10, 11, 12]          1 (1) [2]
+#                                 0 (1) [99]
+
+def build_tree(items, tree=None):
+    while items:
+        children = items.pop(0)
+        num_meta = items.pop(0)
+
+        node = {
+            'num_meta': num_meta,
+            'children': [],
+            'meta': None,
+        }
+        
+        if tree is None:
+            tree = node
+
+        for i in range(children):
+            child_node = build_tree(items, tree=node)
+            node['children'].append(child_node)
+
+        if num_meta > 0:
+            node['meta'] = items[0:num_meta]
+            del items[0:num_meta]
+            return node
+
+def get_meta(node):
+    output = 0
+    if node['children']:
+        for m in node['meta']:
+            try: 
+                output += get_meta(node['children'][m-1])
+            except IndexError:
+                continue
+    else:
+        output = sum(node['meta'])
+
+    return output
+
+items = list(map(lambda x: int(x), inputs[0].split(' ')))
+tree = build_tree(items)
+answer = get_meta(tree)
+print(answer)
