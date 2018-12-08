fd899bd0403bd59cbcee50c53d3df5fd1a1276d6
Thalida Noel <thalida.c.noel@gmail.com>
2018-12-07 21:46:21 -0500

    pair coding with eddy

--- 2018/7/1/run.py
+++ 2018/7/1/run.py
@@ -7,8 +7,19 @@ import os
 
 script_dir = os.path.dirname(__file__)
 inputs = []
-with open(os.path.join(script_dir,'input.txt'), 'r') as f:
+# with open(os.path.join(script_dir,'input.txt'), 'r') as f:
+with open(os.path.join(script_dir,'eddy-input.txt'), 'r') as f:
   inputs = f.read().splitlines()
+
+# inputs = [
+# "Step G must be finished before step A can begin.",
+# "Step C must be finished before step F can begin.",
+# "Step A must be finished before step B can begin.",
+# "Step A must be finished before step D can begin.",
+# "Step B must be finished before step E can begin.",
+# "Step D must be finished before step E can begin.",
+# "Step F must be finished before step E can begin.",
+# ]
 
 def run(inputs):  
     regex = r"Step\s(.{1}).*step\s(.{1})"
@@ -25,10 +36,7 @@ def run(inputs):
     potential_next_steps = all_steps - depends_on_something
     completed_steps = set()
     order = []
-    while True:
-        if len(potential_next_steps) == 0:
-            break;
-        
+    while potential_next_steps:
         step = None
         sorted_potential = sorted(potential_next_steps)
         available_steps = []
