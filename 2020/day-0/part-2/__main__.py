d28f1a63e266a14f07693b7ced70023013e187ac
Thalida Noel <thalida.c.noel@gmail.com>
2020-12-01 12:10:57 -0500

    Setup 2020 Folder & Readme

---
+++ 2020/day-0/part-2/__main__.py
@@ -0,0 +1,27 @@
+# https://adventofcode.com/2020/day/1
+
+import os
+from pprint import pprint
+
+SCRIPT_DIR = os.path.dirname(__file__)
+INPUT_FILENAME = 'inputs.txt'
+
+def get_inputs(dir, filename):
+  file = os.path.join(dir, filename)
+  inputs = []
+
+  with open(file, 'r') as f:
+    inputs = f.read().splitlines()
+
+  return inputs
+
+def process(inputs):
+  outputs = inputs.copy()
+  return outputs
+
+def main():
+  inputs = get_inputs(SCRIPT_DIR, INPUT_FILENAME)
+  answer = process(inputs)
+  print(f'answer:', answer)
+
+main()
---
+++ 2020/shared/day_zero.py
@@ -0,0 +1,7 @@
+# https://adventofcode.com/2019/day/7
+
+import os
+
+class DayZero():
+  def __init__(self):
+    pass
