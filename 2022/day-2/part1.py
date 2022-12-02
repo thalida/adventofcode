0f15735587169621a6f06c5d90a77339e95d77a9
Thalida Noel <thalida.c.noel@gmail.com>
2022-12-02 00:19:24 -0500

    day2: quick, but bad

--- 2022/day-2/part1.py
+++ 2022/day-2/part1.py
@@ -19,16 +19,42 @@ def get_inputs(filename=INPUT_FILENAME):
 
 
 def process(inputs):
-  outputs = inputs.copy()
-  return outputs
+  play_score = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3,
+  }
+  play_type_map = {
+    'A': 'rock',
+    'B': 'paper',
+    'C': 'scissors',
+    'X': 'rock',
+    'Y': 'paper',
+    'Z': 'scissors',
+  }
+  outputs = []
+  for i, input in enumerate(inputs):
+    print(f'input {i}:', input)
+    [y, m] = input.split(' ')
+    score = play_score[m]
+    if (play_type_map[y] == play_type_map[m]):
+      score += 3
+    elif (m == 'X' and y == 'C') or (m == 'Y' and y == 'A') or (m == 'Z' and y == 'B'):
+      score += 6
+
+
+    outputs.append(score)
+
+  print(f'outputs:', outputs)
+  return sum(outputs)
 
 
 test_inputs = get_inputs(filename=SAMPLE_INPUTS_FILENAME)
 test_answer = process(test_inputs)
 print(f'test answer:', test_answer)
-assert test_answer == []
+assert test_answer == 15
 
 inputs = get_inputs(filename=INPUT_FILENAME)
 answer = process(inputs)
 print(f'answer:', answer)
-assert answer == []
+assert answer == 17189
--- 2022/day-2/part2.py
+++ 2022/day-2/part2.py
@@ -19,16 +19,73 @@ def get_inputs(filename=INPUT_FILENAME):
 
 
 def process(inputs):
-  outputs = inputs.copy()
-  return outputs
+  play_score = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3,
+  }
+  play_type_map = {
+    'A': 'rock',
+    'B': 'paper',
+    'C': 'scissors',
+    'X': 'rock',
+    'Y': 'paper',
+    'Z': 'scissors',
+  }
+  outputs = []
+  for i, input in enumerate(inputs):
+    print(f'input {i}:', input)
+    [move, outcome] = input.split(' ')
+
+    score = 0
+    if outcome == 'Y':
+      score += 3
+    elif outcome == 'Z':
+      score += 6
+
+    my_move = ''
+    if score == 0:
+      if move == 'A':
+        my_move = 'Z'
+      elif move == 'B':
+        my_move = 'X'
+      elif move == 'C':
+        my_move = 'Y'
+
+    elif score == 3:
+      if move == 'A':
+        my_move = 'X'
+      elif move == 'B':
+        my_move = 'Y'
+      elif move == 'C':
+        my_move = 'Z'
+
+    elif score == 6:
+      if move == 'A':
+        my_move = 'Y'
+      elif move == 'B':
+        my_move = 'Z'
+      elif move == 'C':
+        my_move = 'X'
+
+    score += play_score[my_move]
+    # if (play_type_map[y] == play_type_map[m]):
+    #   score += 3
+    # elif (m == 'X' and y == 'C') or (m == 'Y' and y == 'A') or (m == 'Z' and y == 'B'):
+    #   score += 6
+
+    outputs.append(score)
+
+  print(f'outputs:', outputs)
+  return sum(outputs)
 
 
 test_inputs = get_inputs(filename=SAMPLE_INPUTS_FILENAME)
 test_answer = process(test_inputs)
 print(f'test answer:', test_answer)
-assert test_answer == []
+assert test_answer == 12
 
 inputs = get_inputs(filename=INPUT_FILENAME)
 answer = process(inputs)
 print(f'answer:', answer)
-assert answer == []
+assert answer == 13490
