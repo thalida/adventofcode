b0c87f258e5c20a5ac17c58be2bab7c9957ec3cf
Thalida Noel <thalida.c.noel@gmail.com>
2022-12-09 01:10:15 -0500

    Day 09

--- 2022/day-09/part1.py
+++ 2022/day-09/part1.py
@@ -17,52 +17,47 @@ def get_inputs(filename=INPUT_FILENAME):
 def process(inputs):
   outputs = inputs.copy()
 
-  head_poses = ['0,0']
-  tail_poses = ['0,0']
+  head_poses = [[0, 0]]
+  tail_poses = [[0, 0]]
 
   for line in inputs:
     direction, amount = line[0], int(line[1:])
 
     for i in range(1, amount + 1):
-      new_head = list(map(int, head_poses[-1].split(',')))
+      head = head_poses[-1].copy()
       if direction == 'L':
-        new_head[0] -= 1
+        head[0] -= 1
       elif direction == 'R':
-        new_head[0] += 1
+        head[0] += 1
       elif direction == 'U':
-        new_head[1] += 1
+        head[1] += 1
       elif direction == 'D':
-        new_head[1] -= 1
+        head[1] -= 1
 
-      head_poses.append(f'{new_head[0]},{new_head[1]}')
+      head_poses.append(head)
 
-      next_tail = list(map(int, tail_poses[-1].split(',')))
+      tail = tail_poses[-1].copy()
 
-      x_diff = new_head[0] - next_tail[0]
-      y_diff = new_head[1] - next_tail[1]
+      dx = head[0] - tail[0]
+      dy = head[1] - tail[1]
 
-      same_col = new_head[0] == next_tail[0]
-      same_row = new_head[1] == next_tail[1]
-      is_diagonal = (
-        not same_col and not same_row and
-        (abs(x_diff) >= 2 or abs(y_diff) >= 2)
+      is_same_col = head[0] == tail[0]
+      is_same_row = head[1] == tail[1]
+      should_move_diag = (
+        not is_same_col and
+        not is_same_row and
+        abs(dx) + abs(dy) > 2
       )
 
-      if is_diagonal or abs(x_diff) >= 2:
-        if x_diff > 0:
-          next_tail[0] += 1
-        else:
-          next_tail[0] -= 1
+      if should_move_diag or abs(dx) > 1:
+        tail[0] += 1 if dx > 0 else -1
 
-      if is_diagonal or abs(y_diff) >= 2:
-        if y_diff > 0:
-          next_tail[1] += 1
-        else:
-          next_tail[1] -= 1
+      if should_move_diag or abs(dy) > 1:
+        tail[1] += 1 if dy > 0 else -1
 
-      tail_poses.append(f'{next_tail[0]},{next_tail[1]}')
+      tail_poses.append(tail)
 
-  return len(set(tail_poses))
+  return len(set(map(lambda x: f'{x[0]},{x[1]}', tail_poses)))
 
 
 test_inputs = get_inputs(filename=SAMPLE_INPUTS_FILENAME)
--- 2022/day-09/part2.py
+++ 2022/day-09/part2.py
@@ -17,54 +17,50 @@ def get_inputs(filename=INPUT_FILENAME):
 def process(inputs):
   outputs = inputs.copy()
 
-  knots = [['0,0'] for _ in range(10)]
-  print(f'knots:', knots)
+  num_knots = 10
+  knots = { i: [[0, 0]] for i in range(num_knots) }
 
   for line in inputs:
     direction, amount = line[0], int(line[1:])
 
     for i in range(1, amount + 1):
-      new_head = list(map(int, knots[0][-1].split(',')))
-      if direction == 'L':
-        new_head[0] -= 1
-      elif direction == 'R':
-        new_head[0] += 1
-      elif direction == 'U':
-        new_head[1] += 1
-      elif direction == 'D':
-        new_head[1] -= 1
+      head = knots[0][-1].copy()
 
-      knots[0].append(f'{new_head[0]},{new_head[1]}')
+      if direction == 'L':
+        head[0] -= 1
+      elif direction == 'R':
+        head[0] += 1
+      elif direction == 'U':
+        head[1] += 1
+      elif direction == 'D':
+        head[1] -= 1
+
+      knots[0].append(head)
 
       for j in range(1, len(knots)):
-        knot_head = list(map(int, knots[j - 1][-1].split(',')))
-        next_tail = list(map(int, knots[j][-1].split(',')))
+        prev_knot = knots[j - 1][-1]
+        curr_knot = knots[j][-1].copy()
 
-        x_diff = knot_head[0] - next_tail[0]
-        y_diff = knot_head[1] - next_tail[1]
+        dx = prev_knot[0] - curr_knot[0]
+        dy = prev_knot[1] - curr_knot[1]
 
-        same_col = knot_head[0] == next_tail[0]
-        same_row = knot_head[1] == next_tail[1]
-        is_diagonal = (
-          not same_col and not same_row and
-          (abs(x_diff) >= 2 or abs(y_diff) >= 2)
+        is_same_col = prev_knot[0] == curr_knot[0]
+        is_same_row = prev_knot[1] == curr_knot[1]
+        should_move_diag = (
+          not is_same_col and
+          not is_same_row and
+          abs(dx) + abs(dy) > 2
         )
 
-        if is_diagonal or abs(x_diff) >= 2:
-          if x_diff > 0:
-            next_tail[0] += 1
-          else:
-            next_tail[0] -= 1
+        if should_move_diag or abs(dx) > 1:
+          curr_knot[0] += 1 if dx > 0 else -1
 
-        if is_diagonal or abs(y_diff) >= 2:
-          if y_diff > 0:
-            next_tail[1] += 1
-          else:
-            next_tail[1] -= 1
+        if should_move_diag or abs(dy) > 1:
+          curr_knot[1] += 1 if dy > 0 else -1
 
-        knots[j].append(f'{next_tail[0]},{next_tail[1]}')
+        knots[j].append(curr_knot)
 
-  return len(set(knots[9]))
+  return len(set(map(lambda p: f'{p[0]},{p[1]}', knots[9])))
 
 
 test_inputs = get_inputs(filename=SAMPLE_INPUTS_FILENAME)
