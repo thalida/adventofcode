51b2b31f08ecb91c124e86d5f821956705355cd7
Thalida Noel <hello@thalida.com>
2023-12-01 04:45:01 +0000

    Setup 2023 :D

--- 2023/templates/day/part1.py
+++ 2023/templates/day/part1.py
@@ -1,17 +1,21 @@
-import os
-from pprint import pprint
+from pathlib import Path
 
-SCRIPT_DIR = os.path.dirname(__file__)
+from rich import pretty, print
+
+pretty.install()
+
+
+SCRIPT_DIR = Path(__file__).resolve().parent
 INPUT_FILENAME = "inputs.txt"
 SAMPLE_INPUTS_FILENAME = "inputs_sample.txt"
 
 
 def get_inputs(filename=INPUT_FILENAME):
-    filepath = os.path.join(SCRIPT_DIR, filename)
+    filepath = Path(SCRIPT_DIR, filename)
     with open(filepath, "r") as f:
         inputs = f.read().splitlines()
 
-    return list(inputs)
+    return inputs
 
 
 def process(inputs):
@@ -21,10 +25,10 @@ def process(inputs):
 
 test_inputs = get_inputs(filename=SAMPLE_INPUTS_FILENAME)
 test_answer = process(test_inputs)
-print(f"test answer:", test_answer)
+print("test answer:", test_answer)
 assert test_answer == []
 
 inputs = get_inputs(filename=INPUT_FILENAME)
 answer = process(inputs)
-print(f"answer:", answer)
+print("answer:", answer)
 assert answer == []
